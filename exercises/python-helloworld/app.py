57c44a0e8a9203e1abd1e7710d0c6e649f26d604
Rohan Kumara <hmrohankumara@gmail.com>
2021-06-12 13:27:28 +0530

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/status")
+def status():
+    return jsonify(result="OK - healthy"), 200
+
+
+@app.route("/metrics")
+def metrics():
+    return jsonify(data={"UserCount": 140, "UserCountActive": 23}), 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
