915b73a68cfa7d3ca207156dd50d618816690162
Rohan Kumara <hmrohankumara@gmail.com>
2021-06-13 07:21:11 +0530

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,23 +1,27 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, logging
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    app.logger.info('Status request successful')
     return "Hello World!"
 
 
 @app.route("/status")
 def status():
+    app.logger.info('Status request successful')
     return jsonify(result="OK - healthy"), 200
 
 
 @app.route("/metrics")
 def metrics():
+    app.logger.info('Metrics request successful')
     return jsonify(data={"UserCount": 140, "UserCountActive": 23}), 200
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
